cfd46b9230e1872b1923840ebe8a68bb689f5dd1
Mona <montwor@wp.pl>
2017-10-11 14:13:40 +0200

    Math work

--- CrystalReign/Assets/Player/playerController.cs
+++ CrystalReign/Assets/Player/playerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     Rigidbody rb;
-    public float speed = 10;
-    public float speedJump = 3;
+    public float rotate = 5;
+    public float speed = 0.5f;
+    public float speedJump = 30;
     bool isGround = false;
     Vector3 currentRotation;
 
@@ -34,14 +35,11 @@ public class playerController : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y") * (-1);
-        float help = Input.GetAxis("Horizontal");
-        float velocityX = (help == 0 ? 0 : (help > 0 ? rb.velocity.x + transform.forward.normalized.x * speed * (-1) : rb.velocity.x + transform.forward.normalized.x * speed));//TODO: nie przeleciec przez podloge
-        float velocityY = rb.velocity.y + (isGround ? Input.GetAxis("Jump") * speedJump : 0);
-        help = Input.GetAxis("Vertical");
-        float velocityZ = (help == 0 ? 0 : (help > 0 ? rb.velocity.z + transform.forward.normalized.z * speed : transform.forward.normalized.z * speed * (-1)));
-        rb.velocity = new Vector3(velocityX, velocityY, velocityZ);
+        Vector3 vector = transform.right * speed * Input.GetAxis("Horizontal") + transform.forward * speed * Input.GetAxis("Vertical");
+        vector.y = (isGround ? Input.GetAxis("Jump") * speedJump : 0);
+        rb.AddForce(vector.normalized, ForceMode.VelocityChange);
         Vector3 rotateValue = new Vector3(mouseY, mouseX, 0);
-        currentRotation += rotateValue;
+        currentRotation += rotateValue*rotate;
         Quaternion rotation = Quaternion.Euler(currentRotation);
         transform.rotation = rotation;
     }
