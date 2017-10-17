4a61b691717151e84bcad741e578ad234402e51e
Unknown <krzstrzelecki@hotmail.com>
2017-10-17 17:34:41 +0200

    Divide objects to global grid

--- Scripts/object_div.py
+++ Scripts/object_div.py
@@ -16,24 +16,28 @@ import numpy
 
 
 
+def round_up(value, step):
+    return step * (1 + (int)(value / step))
+
 def cut_new_chunk(source_shape, big_cube):
     new_obj = source_shape.copy()
     new_obj.data = source_shape.data.copy()
     bpy.context.scene.objects.link(new_obj)
+    bpy.context.scene.update()
 
     source_shape.select = True
     cut1 = source_shape.modifiers.new(name='Cut1', type='BOOLEAN')
     cut1.object = big_cube
     cut1.operation = 'DIFFERENCE'
     bpy.context.scene.objects.active = source_shape
-    bpy.ops.object.convert(target="MESH")
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Cut1")
 
     new_obj.select = True
     cut2 = new_obj.modifiers.new(name='Cut2', type='BOOLEAN')
     cut2.object = big_cube
     cut2.operation = 'INTERSECT'
     bpy.context.scene.objects.active = new_obj
-    bpy.ops.object.convert(target="MESH")
+    bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Cut2")
 
     return new_obj
 
@@ -41,6 +45,7 @@ def cut_new_chunk(source_shape, big_cube):
 def cut(context):
 
     step = 0.7
+    cube_radius=100
 
     # get main object
     obj = bpy.context.scene.objects.active
@@ -52,45 +57,41 @@ def cut(context):
     width = obj.dimensions.y
     depth = obj.dimensions.x
 
-    bpy.ops.mesh.primitive_cube_add()
+    bpy.ops.mesh.primitive_cube_add(radius=cube_radius)
     cube = bpy.context.scene.objects.active
-    cube.scale = (100, 100, 100)
 
     # chunks array
     chunks = []
 
     # dividing with z-translation
-    cube.location = (center.x, center.y, center.z + 100 + height / 2)
+    z_pos = center.z + cube_radius + height / 2
+    cube.location = (center.x, center.y, round_up(z_pos,step))
 
-    i = step
-    while i < height:
+    while cube.location.z - cube_radius > center.z - height/2:
         cube.location.z -= step
         chunks.append(cut_new_chunk(obj, cube))
-        i += step
 
     chunks.append(obj)
 
     # dividing with y-translation
-    cube.location = (center.x, center.y + 100 + width / 2, center.z)
+    y_pos = center.y + cube_radius + width / 2
+    cube.location = (center.x, round_up(y_pos,step), center.z)
 
-    i = step
     chunks_number = len(chunks)
-    while i < width:
+    while cube.location.y - cube_radius > center.y - width/2:
         cube.location.y -= step
         for k in range(0, chunks_number):
             chunks.append(cut_new_chunk(chunks[k], cube))
-        i += step
 
     # dividing with x-translation
-    cube.location = (center.x + 100 + depth / 2, center.y, center.z)
+    x_pos = center.x + cube_radius + depth / 2
+    cube.location = (round_up(x_pos,step), center.y, center.z)
 
-    i = step
     chunks_number = len(chunks)
-    while i < depth:
+    while cube.location.x - cube_radius > center.x - depth/2:
         cube.location.x -= step
         for k in range(0, chunks_number):
             chunks.append(cut_new_chunk(chunks[k], cube))
-        i += step
 
     # delete mega cube
     bpy.ops.object.select_all(action='DESELECT')
@@ -98,12 +99,12 @@ def cut(context):
     bpy.ops.object.delete()
 
     # output array
-    # out = numpy.zeros((int(depth/step)+2, int(width/step)+2, int(height/step)+2))
-    # for chunk in chunks:
-    #    bpy.context.scene.objects.active = chunk
-    #    chunk.select = True
-    #    bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
-    #    out[int((chunk.location.x - (center.x - depth/2))/step), int((chunk.location.y - (center.y - width/2))/step), int((chunk.location.z - (center.z - height/2))/step)] = 1
+    out = numpy.zeros((int(depth/step)+2, int(width/step)+2, int(height/step)+2))
+    for chunk in chunks:
+       bpy.context.scene.objects.active = chunk
+       chunk.select = True
+       bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
+       out[int((chunk.location.x - (center.x - depth/2))/step), int((chunk.location.y - (center.y - width/2))/step), int((chunk.location.z - (center.z - height/2))/step)] = 1
     return ""
 
 
@@ -120,7 +121,7 @@ class CutObject(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class RNObjectCutterPanel(bpy.types.Panel):
+class ObjectCutterPanel(bpy.types.Panel):
     bl_label = "Object Cutter"
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
