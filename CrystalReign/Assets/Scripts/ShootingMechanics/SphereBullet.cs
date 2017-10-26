7fc3bcd63c3cb1281fc50f9c534f606500291f9e
krzywus <filip.krzywy@gmail.com>
2017-10-26 18:46:06 +0200

    added bullets impl

---
+++ CrystalReign/Assets/Scripts/ShootingMechanics/SphereBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace ShootingMechanics
+{
+    public class SphereBullet : Bullet
+    {
+        public float SphereRadius;
+        
+        protected override List<EffectConsumer> GetHitConsumers(Collision other)
+        {
+            Vector3 average = Vector3.zero;
+            other.contacts.ToList().ForEach(x => average += x.point);
+            average /= other.contacts.Length;
+            return Physics.OverlapSphere(average, SphereRadius)
+                .Where(x => x.GetComponent<EffectConsumer>() != null)
+                .Select(x => x.GetComponent<EffectConsumer>())
+                .ToList();
+        }
+    }
+}
