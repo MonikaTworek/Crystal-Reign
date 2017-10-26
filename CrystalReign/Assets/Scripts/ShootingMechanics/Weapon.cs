eed05116a6c61a0a25700a1681945139a8c4cf69
krzywus <filip.krzywy@gmail.com>
2017-10-26 18:45:40 +0200

    basic game interfaces

---
+++ CrystalReign/Assets/Scripts/ShootingMechanics/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Gun : MonoBehaviour
+{
+
+    public virtual void Shoot(Vector3 direction)
+    {
+        Shoot(transform.position, direction);
+    }
+    
+    public abstract void Shoot(Vector3 origin, Vector3 direction);
+}
