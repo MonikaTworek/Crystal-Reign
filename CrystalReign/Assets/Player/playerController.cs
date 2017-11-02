fa7d8f282d2e13ab155d9e3bae3178202272a14d
Mona <montwor@wp.pl>
2017-11-02 22:34:14 +0100

    Repair camera script - "camera colliider"

--- CrystalReign/Assets/Player/playerController.cs
+++ CrystalReign/Assets/Player/playerController.cs
@@ -4,41 +4,55 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    //Rigidbody rb;
     public float speed = 10;
-    public float speedJump = 8;
-    //bool isGround = false;
+    public float speedJump = 7;
     public float gravity = 20;
     private Vector3 moveDirection = Vector3.zero;
+    bool isGround = false;
+    Rigidbody rb;
+
 
     void Start()
     {
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.collider.CompareTag("ground"))
-    //    {
-    //        isGround = true;
-    //    }
-    //}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+        {
+            isGround = true;
+        }
+    }
 
-    //private void OnCollisionExit(Collision collision)
-    //{
-    //    if (collision.collider.CompareTag("ground"))
-    //        isGround = false;
-    //}
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+            isGround = false;
+    }
 
-    
-    
     void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection).normalized;
+        moveDirection *= speed;
+        if (Input.GetButtonDown("Jump") && isGround)
+            moveDirection.y = speedJump;
+        //moveDirection.y -= gravity;
+        moveDirection.y += rb.velocity.y;
+        rb.velocity = moveDirection;
+        
+
+
+    }   
+    
+    void check()
     {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = transform.TransformDirection(moveDirection).normalized;
             moveDirection *= speed;
             if (Input.GetButton("Jump"))
                 moveDirection.y = speedJump;
