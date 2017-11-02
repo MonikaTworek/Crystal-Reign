b0f68b8d81e5c20cc24bdedb89599dd72ac72a2c
krzywus <filip.krzywy@gmail.com>
2017-11-02 19:42:43 +0100

    bullets expiration after hit

--- CrystalReign/Assets/Scripts/ShootingMechanics/Bullets/Bullet.cs
+++ CrystalReign/Assets/Scripts/ShootingMechanics/Bullets/Bullet.cs
@@ -6,22 +6,48 @@ public abstract class Bullet : MonoBehaviour {
     public Effect effect { get; set; }
     public bool ShouldBeRemoved = true;
     
+    private bool isAfterCollision;
+    private static float maxTimer = 96f;
+    private int timer = (int) maxTimer;
+    
+    void Update()
+    {
+        if (isAfterCollision && ShouldBeRemoved)
+        {
+            if (timer <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                AddTransparency();
+                timer--;
+            }
+        }
+    }
+
+    private void AddTransparency()
+    {
+        Color currentColor = gameObject.GetComponent<MeshRenderer>().material.color;
+        currentColor.a = 1f - (maxTimer - timer) / maxTimer;
+        gameObject.GetComponent<MeshRenderer>().material.color = currentColor;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
+        if (isAfterCollision)
+        {
+            return;
+        }
+        isAfterCollision = true;
         foreach (EffectConsumer effectConsumer in GetHitConsumers(other))
         {
             ApplyEffectOnConsumer(effectConsumer);  
         }
-        if (ShouldBeRemoved)
-        {
-           Destroy(gameObject);
-        }
-        else
-        {
-            Destroy(this);
-        }
-    }
+        gameObject.GetComponent<Rigidbody>().useGravity = true;
 
+    }
+    
     protected abstract List<EffectConsumer> GetHitConsumers(Collision other);
 
     private void ApplyEffectOnConsumer(EffectConsumer effectConsumer)
