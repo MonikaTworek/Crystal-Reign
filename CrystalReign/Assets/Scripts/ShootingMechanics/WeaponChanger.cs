57ee4c5fb78d1015658d066ccce907cc4e88041b
krzywus <filip.krzywy@gmail.com>
2017-11-09 21:47:41 +0100

    introduced weapon changer

---
+++ CrystalReign/Assets/Scripts/ShootingMechanics/WeaponChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponChanger : MonoBehaviour
+{
+	public List<Weapon> Weapons;
+
+	private int CurrentWeaponIndex;
+	
+	public Weapon GetPreviousWeapon()
+	{
+		CurrentWeaponIndex--;
+		if (CurrentWeaponIndex < 0)
+		{
+			CurrentWeaponIndex = Weapons.Capacity + CurrentWeaponIndex;
+		}
+		return Weapons.ToArray()[CurrentWeaponIndex];
+	}
+	
+	public Weapon GetNextWeapon()
+	{
+		CurrentWeaponIndex++;
+		return Weapons.ToArray()[CurrentWeaponIndex % Weapons.Capacity];
+	}
+	
+}
