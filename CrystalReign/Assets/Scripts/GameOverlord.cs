572863d2cbf778c4c084d1cbe59e0afd0610b887
krzywus <filip.krzywy@gmail.com>
2017-12-07 21:51:44 +0100

    game overlord script

---
+++ CrystalReign/Assets/Scripts/GameOverlord.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GameOverlord : MonoBehaviour
+{
+
+	public double playerHitPoints;
+	public double ammunitionLeft;
+	public Weapon selectedWeapon;
+	public WeaponChanger WeaponChanger;
+	
+	
+	public void processMessage(OverlordMessage message, double value)
+	{
+		switch (message)
+		{
+			case OverlordMessage.CHANGE_PLAYER_HIT_POINTS:
+			{
+				playerHitPoints += value;
+				break;
+			}
+			case OverlordMessage.CHANGE_AMMUNITION:
+			{
+				ammunitionLeft += value;
+				break;
+			}
+			case OverlordMessage.CHANGE_WEAPON:
+			{
+				selectedWeapon = value > 0 ? 
+					WeaponChanger.GetNextWeapon() : 
+					WeaponChanger.GetPreviousWeapon();
+				break;
+			}
+		}	
+	}
+}
+
+public enum OverlordMessage
+{
+	CHANGE_PLAYER_HIT_POINTS,
+	CHANGE_AMMUNITION,
+	CHANGE_WEAPON
+}
