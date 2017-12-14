55fef3a1e71e7f44437c6bc419e31e7e83648b9d
Creestoph <krzysztof-strzelecki@hotmail.com>
2017-12-14 23:53:39 +0100

    HpBar smooth

--- CrystalReign/Assets/Scripts/HUD/HPBar.cs
+++ CrystalReign/Assets/Scripts/HUD/HPBar.cs
@@ -7,17 +7,25 @@ public class HPBar : MonoBehaviour {
     private RectTransform rt;
     private RectTransform rtParent;
 
+    private float targetWidth;
+    [Range(0,1)]
+    public float speed = 0.7f;
 
     // Use this for initialization
     void Start () {
         rt = transform.GetChild(0).GetComponent<RectTransform>();
         rtParent = GetComponent<RectTransform>();
-
-        setHP(1);
+        targetWidth = rt.sizeDelta.x;
 	}
 	
 	public void setHP(float value)
     {
-        rt.sizeDelta = new Vector2(rtParent.sizeDelta.x * value, rt.sizeDelta.y);
+        targetWidth = rtParent.sizeDelta.x * value;
     }
+
+    public void FixedUpdate()
+    {
+        rt.sizeDelta = new Vector2(Mathf.Lerp(rt.sizeDelta.x, targetWidth, speed), rt.sizeDelta.y);
+    }
+
 }
