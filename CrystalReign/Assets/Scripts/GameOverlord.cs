a9b2bae622657d890bf8b0fe86384c90dceb376b
Creestoph <krzysztof-strzelecki@hotmail.com>
2017-12-14 20:24:52 +0100

    Lighting fix

--- CrystalReign/Assets/Scripts/GameOverlord.cs
+++ CrystalReign/Assets/Scripts/GameOverlord.cs
@@ -8,11 +8,11 @@ public class GameOverlord : MonoBehaviour
 	public double AmmunitionLeft = 100;
 	public Weapon SelectedPlayerWeapon;
 	public WeaponChanger WeaponChanger;
-	public Text GUIPlayerHitPoints;
+	//public Text GUIPlayerHitPoints;
 
 	void Start()
 	{
-		GUIPlayerHitPoints.text = PlayerHitPoints.ToString("00.00");
+		//GUIPlayerHitPoints.text = PlayerHitPoints.ToString("00.00");
 	}
 	
 	public void processMessage(OverlordMessage message, double value)
@@ -22,7 +22,7 @@ public class GameOverlord : MonoBehaviour
 			case OverlordMessage.CHANGE_PLAYER_HIT_POINTS:
 			{
 				PlayerHitPoints += value;
-				GUIPlayerHitPoints.text = PlayerHitPoints.ToString("00.00");
+				//GUIPlayerHitPoints.text = PlayerHitPoints.ToString("00.00");
 				break;
 			}
 			case OverlordMessage.CHANGE_AMMUNITION:
